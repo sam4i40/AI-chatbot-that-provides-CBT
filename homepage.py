ef6c8085847065e077ad8848bcaae6290c91e53b
Nasrullah Sami <52885328+sam4i40@users.noreply.github.com>
2025-06-17 03:58:45 +0600

    Update homepage.py
    Updated `html_string`

--- homepage.py
+++ homepage.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-html_string = "<body><h1>Write, edit and run HTML, CSS and JavaScript code online.</h1><p>Our HTML editor updates the webview automatically in real-time as you write code.</p></body>"
+html_string = "<body><h1>Do you need someone to listen to you? We are here to listen to you!</h1><p>Chat with me about anything you need. I am all ears.</p></body>"
 
 st.markdown(html_string, unsafe_allow_html = True);
